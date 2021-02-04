5fbd24cc3c06f6120f9bc525dac88fa62e7f3d08
TaibiNadia <46710778+TaibiNadia@users.noreply.github.com>
2021-02-04 15:15:38 +0100

    Update funtest.py

--- tests-fun/funtest.py
+++ tests-fun/funtest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 import unittest
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
+import xmlrunner
 
 class LoginTest(unittest.TestCase):
     
@@ -20,34 +20,33 @@ class LoginTest(unittest.TestCase):
         self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=opts)
         self.driver.maximize_window()
         self.driver.implicitly_wait(30)
-        self.driver.save_screenshot("toto.png")
+        #self.driver.save_screenshot("toto.png")
 
     def test_login_valid (self):
-        self.driver.get("http://prestashop-git/fr/connexion?back=my-account")
+        self.driver.get("http://10.10.20.71:8001/login?back=my-account")
         self.driver.find_element_by_name("email").send_keys("mm@gmail.com")
         self.driver.find_element_by_name("password").send_keys("MM@3841")
         self.driver.find_element_by_id("submit-login").click()
+        self.assertNotIn("Authentication failed.", self.driver.page_source)
         self.driver.find_element_by_class_name("logout").click()
         time.sleep(2)
         
     def test_search_page (self):
-        #wait = WebDriverWait(self.driver, 10)
         search_query = "mug"
-        self.driver.get("http://prestashop-git/")
+        self.driver.get("http://10.10.20.71:8001/")
         self.driver.find_element_by_name("s").send_keys(search_query)
         self.driver.find_element_by_class_name("material-icons").click()
-        #wait.until(presence_of_element_located((By.ID, "js-product-list")))
         self.driver.find_elements_by_id("js-product-list")
-        #results
         results = self.driver.find_elements_by_class_name("product-miniature")
-        for article in results:
-            print(article.text)
-            print()
+        self.assertTrue(results)
+        #for article in results:
+        #    print(article.text)
+        #    print()
     
     def tearDown(self):
         self.driver.quit()
         print("Test completed")
 
 if __name__== '__main__':
-
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reports'))
     unittest.main()
